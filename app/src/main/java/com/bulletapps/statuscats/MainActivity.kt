19860ea3b904286faeba928b74781236084a586a
jvsena42 <jvsena16@gmail.com>
2021-01-30 07:48:13 -0300

    [ADD] MainActivity.kt / activity_main.xml - configuração do data binding

--- app/src/main/java/com/bulletapps/statuscats/MainActivity.kt
+++ app/src/main/java/com/bulletapps/statuscats/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bulletapps.statuscats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bulletapps.statuscats.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }
